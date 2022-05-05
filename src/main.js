7d670943e2e2b5c8a85b1b89b81c62eb852fc197
seokki kwon <kwons4163@naver.com>
2022-05-05 17:29:45 +0900

    파이어베이스 emulator 설정완료

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import '@/assets/css/vertical-spacing.css'; /* css spacing */
-import '../plugins/firebase.js'
+
 
 
 createApp(App).use(router).mount('#app')
